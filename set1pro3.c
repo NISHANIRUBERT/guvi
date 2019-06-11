7d0c6541ed10b9c952e6de2a6e6abc621d99532a
NISHANIRUBERT <51689306+NISHANIRUBERT@users.noreply.github.com>
2019-06-11 10:02:15 +0530

    Create set1pro3.c

---
+++ set1pro3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	// your code goes here
+	char str[100000],str1[100000];
+	int index,index2=0;
+	scanf("%s",str);
+	for(index=0;str[index];len++,index++);
+	for(index=len-1;index>=0;index--)
+	str1[index2++]=str[index];
+	
+	str1[index2]='\0';
+	
+	
+	printf("%s",str1);
+	
+	
+	return 0;
+}
